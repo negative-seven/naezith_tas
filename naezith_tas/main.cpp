3853a28d69c3ccd2ef23e42048d31dca6790113e
negative-seven <tehseven1@gmail.com>
2018-03-10 20:59:21 +0100

    Small fixes to command line parameter

--- naezith_tas/main.cpp
+++ naezith_tas/main.cpp
@@ -8,12 +8,13 @@ int main(int argc, char *argv[])
 
 	for (int i = 1; i < argc; i++)
 	{
-		if (strcmp(argv[i], "--noalloc") == 0)
+		if (strcmp(argv[i], "--no-alloc") == 0)
 		{
 			alloc = false;
 		}
 	}
 
+	gameManager.alloc = alloc;
 	gameManager.run();
 
 	return 0;
